131729acfb844605e381d2f53371d388000f2151
James O <51519722+jo-royal@users.noreply.github.com>
2025-06-26 13:46:51 +0200

    whishlist-footer

--- src/pages/wishlist.jsx
+++ src/pages/wishlist.jsx
@@ -1,14 +1,17 @@
-import React from 'react'
+
 import Navbar from '../components/navbar'
 import PageNav from '../components/pageNav'
 import WishlistCom from '../components/wishlistCom'
+import Footer from '../components/footer'
+
 
 export default function Wishlist() {
   return (
     <div>
-        <Navbar />
-        <PageNav title="Your WishList" subtitle=" Your WishList" />
-        <WishlistCom />
+      <Navbar />
+      <PageNav title="Your WishList" subtitle=" Your WishList" />
+      <WishlistCom />
+      <Footer />
     </div>
   )
 }
