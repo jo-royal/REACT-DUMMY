2a17104888ad418a57737a2939eb08d27fb38f7a
James O <51519722+jo-royal@users.noreply.github.com>
2025-06-26 12:33:41 +0200

    footer-eight

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -8,9 +8,9 @@ export default function Footer() {
                 <div className='flex flex-col gap-6 sm:order-last md:order-first sm:col-span-2 md:col-span-1 sm:text-center md:text-start w-full'>
                     <Link to='/REACT-DUMMY/' className='text-2xl font-header'><span className='text-accent'>PES</span>MIC</Link>
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur facilis iusto ea sit nesciunt architecto eum repellat quibusdam itaque velit fuga, rerum ad natus accusantium.</p>
-                    <div className='flex flex-col gap-4 w-full'>
+                    <div className='flex gap-4 w-full sm:justify-center md:justify-start'>
                         <h6>Follow Us:</h6>
-                        <ul className='flex gap-3 justify-center'>
+                        <ul className='flex gap-3'>
                             {medias.map((media, index) => (
                                 <li key={index}>
                                     <Link to={media.href}>{media.icons}</Link>
