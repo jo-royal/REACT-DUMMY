bba754b51316d81f924d68661a4f213b81e410e0
James O <51519722+jo-royal@users.noreply.github.com>
2025-06-26 13:45:58 +0200

    checkout-footer

--- src/pages/checkout.jsx
+++ src/pages/checkout.jsx
@@ -1,16 +1,17 @@
-import React from 'react'
-import Navbar from '../components/Navbar.jsx'
+
+import Navbar from '../components/navbar.jsx'
 import CheckoutCom from '../components/checkoutCom.jsx'
+import Footer from '../components/footer'
 
 export default function Checkout() {
   return (
     <div>
       <div className='shadow-lg pb-5 sm:pb-5 sm:pt-2'>
-      <Navbar />
+        <Navbar />
       </div>
-        <CheckoutCom / >
+      <CheckoutCom />
+      <Footer />
     </div>
-    
   )
 }
 
