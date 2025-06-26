c599ffd7b8b44eac2c44d2b972b12649dbbf7623
James O <51519722+jo-royal@users.noreply.github.com>
2025-06-26 13:46:29 +0200

    shipping-update footer

--- src/pages/shippingUpdate.jsx
+++ src/pages/shippingUpdate.jsx
@@ -1,14 +1,16 @@
-import React from 'react'
+
 import Navbar from '../components/navbar'
 import UserShipping from '../components/userShipping'
+import Footer from '../components/footer'
 
 export default function ShippingUpdate() {
   return (
     <div>
-        <div className='shadow-lg pb-3 sm:pb-5 sm:pt-2'>
-           <Navbar />
-        </div>
-        <UserShipping />
+      <div className='shadow-lg pb-3 sm:pb-5 sm:pt-2'>
+        <Navbar />
+      </div>
+      <UserShipping />
+      <Footer />
     </div>
   )
 }
